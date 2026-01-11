97fcf6c55d956034bc8250358dfae71ff34a703c
JossJL <joss.lazenby@gmail.com>
2026-01-11 14:44:36 +0000

    Time: 525 ms (36.65%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+with changed_products as(
+    select 
+        product_id, 
+        new_price as change_price
+    from products p1
+    where (product_id, change_date) in (
+        -- Subquery to find the LATEST date for each product
+        select product_id, max(change_date)
+        from products
+        where change_date <= '2019-08-16'
+        group by product_id
+    )
+)
+select
+    p.product_id,
+    ifnull(change_price, 10) as price
+from products p
+left join changed_products c on
+    p.product_id = c.product_id
+group by p.product_id
+
